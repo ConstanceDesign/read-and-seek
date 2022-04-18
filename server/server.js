4a99b6863fe1b2234a1a1a9029950146f2621e17
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-18 12:21:53 -0400

    root

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-
+const application_root = require("./client/public/");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
@@ -30,10 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
-app.use(express.static(application_root));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(application_root));
+  // app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/public"));
