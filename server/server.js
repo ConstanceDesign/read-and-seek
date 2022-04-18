6839287afbe23972ed3741ec790027e1d488251e
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-18 12:18:55 -0400

    path

--- server/server.js
+++ server/server.js
@@ -30,9 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+app.use(express.static(application_root));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/public"));
