52aeeb9eafe290e8c9319e88e0657e16d86a0a37
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-18 10:50:18 -0400

    public

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/"));
+  res.sendFile(path.join(__dirname, "../client/public"));
 });
 
 startServer();
