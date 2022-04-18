97a57bdbe9d85f96bcf9a611189011089ae0a11e
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-18 12:38:03 -0400

    connection

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const path = require("path");
 
-const { typeDefs, resolvers } = require("./server/schemas");
+const { typeDefs, resolvers } = require(".schemas");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
